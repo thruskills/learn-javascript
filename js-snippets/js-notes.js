300570c4ce5907c84a0e7e163e7f559b9c8b733e
Manohar Negi <manohar.negi@gmail.com>
2019-11-15 11:53:09 +0530

    day 5 contents

---
+++ js-snippets/js-notes.js
@@ -0,0 +1,73 @@
+// JavaScript operators
+/*
+
+1. Assignment operators
+    =
+    +=
+    -=
+    *=
+    /=
+    %=
+    <<=
+    >>=
+
+2. Comparison operators
+    ==
+    !=
+    ===
+    !==
+    >
+    >=
+    <
+    <=
+
+3. Arithmatic operatos
+    ++ increment
+    -- decrement 
+
+    pre increment 
+    post increment
+    
+4. Bitwise operator
+    Operands binary values
+
+    AND
+    OR
+    NOT (ONE OPERAND)
+    XOR
+    SHIFT
+
+    X = 2 = 0010
+    Y = 3 = 0011
+    ------------
+        OR  0011  = 3
+        AND 0010  = 2
+        XOR 0001  = 1
+
+    ~ X;
+    ~ 0010  = 1 101 (SIGNE)
+
+    SHIFT
+    << LEFT SHIFT
+        
+        X = 2 = 0010
+
+        X << 1 = 0010 << 2 => 1000
+
+    >> RIGHT SHIFT
+
+        X = 2 = 0010
+        X >> 1 = 0001
+
+    4. Logical Operators
+    
+        AND &&  -  (operand 1) && (operand 2) = true if both true
+        OR  ||  -  (operand 1) && (operand 2) = true if anyone true
+    
+    5. Terneay operator ( ?: )
+
+        expression ? "if true" : "if false"
+
+    
+
+*/
