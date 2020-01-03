fbba030b348fa520537ff63f43249bde14a8cc5c
Manohar Negi <manohar.negi@gmail.com>
2020-01-03 09:40:07 +0530

    added day 23

--- web/30-my-express-app/routes/index.js
+++ web/30-my-express-app/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Hbs' });
+  res.render('index', { title: 'Express Hbs', data: 'This is to demostrate how to pass data to views.' });
 });
+
+/* GET the about page */
+router.get('/about', function(req, res, next){
+  // we will render the about.hbs viw here
+  res.render('about')
+});
+
+/* GET the contact page */
+router.get('/contact', function(req, res, next){
+  res.render('contact')
+});
+
+router.get('/login', function(req, res, next){
+  res.render('login', {layout: 'layout2'})
+})
 
 module.exports = router;
