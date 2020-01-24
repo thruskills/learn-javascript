d2ac933f1e80766e16bd4e532eff68c9dcc8cb2f
Manohar Negi <hello@manoharnegi.com>
2020-01-24 09:26:29 +0530

    added day 37

--- web/34-express-dashboard/routes/dashboard.js
+++ web/34-express-dashboard/routes/dashboard.js
@@ -48,7 +48,7 @@ router.get('/projects',  function(req, res, next) {
     let dbo = db.db("portfolio");
     let d = new Date();
     // get the projects
-    dbo.collection('projects').find({}).limit(3).toArray(function(err, projects){
+    dbo.collection('projects').find({}).limit(10).toArray(function(err, projects){
         if (err) throw err;
         console.log(JSON.stringify(projects));
         db.close();
@@ -60,27 +60,85 @@ router.get('/projects',  function(req, res, next) {
 
 /* Create project */
 router.get('/projects/new',  function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('projects/createProject', {layout: 'layout_dashboard', 'title': 'Create new project'})
 });
 
 /* Submit create project */
 router.post('/projects/new',  function(req, res, next) {
-  res.send('respond with a resource');
+  let title = req.body.title;
+  let image = req.body.image;
+  let description = req.body.description;
+  let project = {title, image, description};
+  
+  // write it to the db
+  MongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    let d = new Date();
+    // get the projects
+    dbo.collection('projects').insertOne(project, function(err, project){
+        if (err) throw err;
+        console.log(JSON.stringify(project));
+        db.close();
+        // redirect to list of projects page
+        res.redirect('/projects')
+    })
+  });
+  
 });
 
 /* Project detail*/
 router.get('/projects/:id',  function(req, res, next) {
-  res.send('respond with a resource');
+  // read the id from the path param
+  let id = req.params.id;
+  MongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    dbo.collection('projects').findOne({_id: new ObjectId(id)}, function(err, project){
+      if (err) throw err;
+      console.log(JSON.stringify(project));
+      db.close();
+      res.render('projects/projectDetail', { project : project , layout: 'layout_dashboard'})
+    })
+  });
 });
 
 /* update project */
-router.put('/projects/:id',  function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/projects/:id',  function(req, res, next) {
+  let id = req.params.id;
+
+  let title = req.body.title;
+  let image = req.body.image;
+  let description = req.body.description;
+  let project = {title, image, description};
+  let updatedProject = {$set: project};
+
+  MongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    dbo.collection('projects').updateOne({_id: new ObjectId(id)}, updatedProject, function(err, p){
+      if (err) throw err;
+      console.log(JSON.stringify(p));
+      db.close();
+      res.render('projects/projectDetail', { project : project , layout: 'layout_dashboard', success: true})
+    })
+  });
+
 });
 
 /* delete project */
-router.delete('/projects/:id',  function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/projects/:id/delete',  function(req, res, next) {
+  let id = req.params.id;
+  MongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    let dbo = db.db("portfolio");
+    dbo.collection('projects').deleteOne({_id: new ObjectId(id)}, function(err, p){
+      if (err) throw err;
+      console.log(JSON.stringify(p));
+      db.close();
+      res.redirect('/projects')
+    })
+  });
 });
 
 router.get('/logout',  function(req, res, next) {
